2db4822c0125bd98faf58c754043ea6fba34729b
carooull <carooull.1412@gmail.com>
2025-06-08 20:33:59 -0300

    Alteração - inclusão de data nas transações e correção de bug

--- sistema_bancario/desafio_dio_sistema_bancario.py
+++ sistema_bancario/desafio_dio_sistema_bancario.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 def menu():
     print(""" ******Bem-vindo ao Sistema do Banco! Por favor escolha uma das opções abaixo: ******
 [1]Sacar
@@ -6,11 +7,10 @@ def menu():
 [0]Sair""")
     
     while True:
-        global saldo, operacoes, tentativas
+        global saldo, operacoes, transacoes
         
         user = input("\nOpção: ")
         if user == "1":
-            tentativas += 1
             return sacar()
         elif user == "2":
             return depositar()
@@ -24,10 +24,9 @@ def menu():
             return menu()
      
 def sacar():
-    global saldo, operacoes
-    max_tentativas = 3   
-
-    while tentativas <= max_tentativas:
+    global saldo, operacoes, transacoes, transacoes_diarias
+    
+    while transacoes < transacoes_diarias:
         valor_saque = int(input("Digite o valor desejado: \n"))
                 
         if valor_saque <= 0:
@@ -41,29 +40,35 @@ def sacar():
             print("Limite por saque de R$ 500 atingido! Por favor tente outro valor. \n")
         else:
             saldo = saldo - valor_saque
-            operacoes.append({"Tipo: ": "Saque ", "valor: R$ ": valor_saque})
+            operacoes.append({"Tipo: ": "Saque ", "valor: R$ ": valor_saque, "Data: ":horario.strftime(horario_ptbr)})
             print(f"Saque realizado com sucesso!! Seu novo saldo é de R${saldo}. \n")
+            transacoes +=1
             return menu()
-    print("Limite de saques diários atingido. Por favor tente novamente amanhã, obrigado!! \n")
+    print("Limite de transações diárias atingido. Por favor tente novamente amanhã, obrigado! ")
     return menu()
         
 def depositar():
-   global saldo, operacoes
-   valor_deposito = int(input("Digite o valor desejado: \n"))
+    global saldo, operacoes, transacoes, transacoes_diarias
+      
+    while transacoes < transacoes_diarias:
+        valor_deposito = int(input("Digite o valor desejado: \n"))
+        if valor_deposito <= 0:
+            print("Operação inválida! \n")
+            return menu()
 
-   if valor_deposito <= 0:
-        print("Operação inválida! \n")
-        return menu()
-
-   else:
-        saldo = saldo + valor_deposito
-        operacoes.append({"Tipo: ": "Deposito ","Valor: R$ ":valor_deposito})
-        print(f"Deposito realizado com sucesso!! Seu novo saldo é de R${saldo}. \n")
-        return menu()
+        else:
+            saldo = saldo + valor_deposito
+            operacoes.append({"Tipo: ": "Deposito ","Valor: R$ ":valor_deposito,"Data: ":horario.strftime(horario_ptbr)})
+            print(f"Deposito realizado com sucesso!! Seu novo saldo é de R${saldo}. \n")
+            transacoes +=1
+            return menu()
+       
+    print("Limite de transações diárias atingido. Por favor tente novamente amanhã, obrigado! ")
+    return menu()
 
 def extrato():
-    global saldo, operacoes
-    print("****************** EXTRATO ****************** \n")
+    
+    print("******************************************** EXTRATO *********************************************\n")
     print("Aqui estão suas últimas operações realizada: \n")
     print(f"Seu saldo atual é de R$ {saldo}.\n")
     if operacoes == []:
@@ -71,17 +76,19 @@ def extrato():
             return menu()
     for i in operacoes:
         print(f"Operação  {i}\n")
+        
+    print("***************************************************************************************************\n")
     
-    return
-
-    
+    return menu()
 
 
 
 
-
-
+horario = datetime.now()
+horario_ptbr ="%d/%m/%Y %H:%M"
 operacoes = []
-tentativas = 0
+transacoes = 0
+transacoes_diarias = 10
 saldo = 1000
+
 menu()
